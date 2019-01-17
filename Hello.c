cfdfe6345e43831974601dffd4dce6fda45b5f87
Stephen Wilson <smwthebuilder@hotmail.com>
2019-01-17 12:54:58 -0600

    Added name & major

---
+++ Hello.c
@@ -0,0 +1,13 @@
+/**
+*  Author: Stephen Wilson
+*  Date: 2019/17/01
+*
+*  A simple hello world program in C
+*
+*/
+#include<stdlib.h>
+#include<stdio.h>
+int main(void) {
+printf("Stephen Wilson Electrical Engineering\n");
+return 0;
+}
